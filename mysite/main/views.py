92fb65d7e584ef15be0fb2da00caeaa055aa7b3a
1001-Ashby-Tanner <tanner.ashby1@yahoo.com>
2021-11-12 15:28:16 -0800

    Setup

---
+++ mysite/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+
+# Create your views here.
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>My Website</h1>\n<h1>%s</h1><p>%s</p>" %(ls.name, str(item.text)))
+    #<br></br> like newline character
+
+def home(response):
+    return HttpResponse("<h1>ashbyweb.tech Landing Page</h1>")
